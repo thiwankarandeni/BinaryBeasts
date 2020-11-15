d1c1feedbe403271e99ec22ab49808d53aaf8651
thiwankarandeni <74093710+thiwankarandeni@users.noreply.github.com>
2020-11-15 17:14:12 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner; 
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Select an option");
+		System.out.println("Enter 1 to display all contacts");
+		System.out.println("Enter 2 to add contacts");
+		System.out.println("Enter 3 to remove contacts");
+		System.out.println("Enter 4 to edit a contacts");
+		System.out.println("Enter 5 to load a contact from a text file");
+		System.out.println("Enter 6 to save contact to the text file");
+		System.out.println("Enter 7 to exit from the program");
+		
+		Scanner myObj = new Scanner(System.in);
+		int i = myObj.nextInt();
+		
+		switch(i) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		
+		}
+
+	}
+
+}
